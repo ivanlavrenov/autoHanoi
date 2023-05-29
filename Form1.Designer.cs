387c4e48c9fdcfa5506f1d1fc8411fc3b31bc20f
Ivan <Viduck27@gmail.com>
2023-05-29 23:42:38 +0300

    fc

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace TowersWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(541, 584);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 99);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(335, 584);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 99);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(136, 584);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1 ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 295);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 171);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(353, 295);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 171);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(551, 295);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 171);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(168, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 57);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Начать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.startClick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1026, 26);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(372, 657);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 743);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TowersWindows
+{
+    public partial class Form1 : Form
+    {
+        private List<string> moves = new List<string>(); // список с действиями
+        private List<List<int>> towers = new List<List<int>>();
+        public Form1()
+        {
+            towers.Add(new List<int>());
+            towers.Add(new List<int>());
+            towers.Add(new List<int>());
+
+            InitializeComponent();
+             
+            for (int i = 1; i <= 3; i++)
+            {
+                towers[0].Add(i);
+            }
+
+            textBox1.Text = string.Join(Environment.NewLine, towers[0]); //Environment.NewLine делает перенос на новую строку
+        }
+
+        private void startClick(object sender, EventArgs e)
+        {
+            int from = 0;
+            int to = 2;
+            int other = 1;
+
+            solveTowers(3, from, to, other);
+        }
+
+
+        private void solveTowers(int n, int from, int to, int other)
+        {
+            // solveTowers->6 -> solveTowers->5 -> solveTowers->4 -> solveTowers->3 -> solveTowers->2 -> solveTowers->1 -> solveTowers->0 end 
+            //                   solveTowers->5 -> solveTowers->4
+            //                                  -> solveTowers->4 
+            //                                  -> solveTowers->4
+            //
+
+            if (n > 0)
+            {
+                solveTowers(n - 1, from, other, to);
+
+                towers[from].Remove(n); //удаляем значение диска в списке башни, откуда взяли диск
+
+                towers[to].Reverse();
+                towers[to].Add(n);
+                towers[to].Reverse();
+
+                moves.Add(string.Format("Диск {0} перемещен с {1} на {2}", n, from + 1, to + 1)); // добавление в список дейстивия
+                
+                
+                //передаем элементу textBox значение списка
+                textBox1.Text = string.Join(Environment.NewLine, towers[0]); //Environment.NewLine делает перенос на новую строку
+                textBox2.Text = string.Join(Environment.NewLine, towers[1]);
+                textBox3.Text = string.Join(Environment.NewLine, towers[2]);
+                textBox4.Text = string.Join(Environment.NewLine, moves);
+
+                Thread.Sleep(200); // таймауты для постепенного отображения
+                textBox1.Refresh(); // обновление компонентов для визуализации действий
+                textBox2.Refresh();
+                textBox3.Refresh();
+                textBox4.Refresh();
+
+                solveTowers(n - 1, other, to, from);
+            }
+        }
+    }
+}
